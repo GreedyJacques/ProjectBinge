c1b1f102d73b159e41362d09b68a96b626284893
Vaidas Lenartavicius <vaidas.lenartavicius@gmail.com>
2020-07-13 21:34:22 +0200

    Added Git vim test line

--- src/launchcode/Main.java
+++ src/launchcode/Main.java
@@ -3,6 +3,7 @@ package launchcode;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Git vim test");
     }
 
 }
