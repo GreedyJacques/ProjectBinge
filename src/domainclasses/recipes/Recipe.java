1bec37b675465cba9e175e8757efa84f9b031731
Vaidas Lenartavicius <vaidas.lenartavicius@gmail.com>
2020-07-15 15:09:24 +0200

    Filled skeleton of Recipe class

--- src/domainclasses/recipes/Recipe.java
+++ src/domainclasses/recipes/Recipe.java
@@ -1,4 +1,69 @@
 package domainclasses.recipes;
 
+import java.util.ArrayList;
+
 public class Recipe {
+    int id;
+    String name;
+    ArrayList<IngredientQty> ingredients;
+    String procedure;
+    int preptime;
+    int cooktime;
+
+    public Recipe() {
+        this.id = -1; //TODO
+        this.name = "New Recipe";
+        this.ingredients = new ArrayList<IngredientQty>();
+        this.procedure = "Procedure goes here";
+        this.preptime = 0;
+        this.cooktime = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<IngredientQty> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<IngredientQty> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getProcedure() {
+        return procedure;
+    }
+
+    public void setProcedure(String procedure) {
+        this.procedure = procedure;
+    }
+
+    public int getPreptime() {
+        return preptime;
+    }
+
+    public void setPreptime(int preptime) {
+        this.preptime = preptime;
+    }
+
+    public int getCooktime() {
+        return cooktime;
+    }
+
+    public void setCooktime(int cooktime) {
+        this.cooktime = cooktime;
+    }
 }
--- src/launchcode/Main.java
+++ src/launchcode/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Git vim test");
         System.out.println("ciao");
+
         Ingredient a = new Ingredient();
         Ingredient b = new Ingredient("Nutella",1,500);
 
