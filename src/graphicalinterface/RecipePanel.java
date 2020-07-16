5e1de66cb1ff541fd731c7e2d3aa80aee152dc4e
Vaidas Lenartavicius <vaidas.lenartavicius@gmail.com>
2020-07-16 18:28:06 +0200

    added JTable functionality to RecipePanel

--- src/graphicalinterface/RecipePanel.java
+++ src/graphicalinterface/RecipePanel.java
@@ -1,36 +1,65 @@
 package graphicalinterface;
 
+import domainclasses.recipes.Recipe;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class RecipePanel extends JPanel implements ActionListener {
 
     public RecipePanel() {
-        super(new MigLayout("fill, wrap 3", "50[][grow,fill]20[]","50[][]20[grow,fill][][][]50"));
+        super(new MigLayout("fill, wrap 3", "50[][grow,fill]20[]", "50[][]20[grow,fill][][][]50"));
 
         JButton addButton = new JButton("ADD");
         JButton removeButton = new JButton("REMOVE");
         JButton openButton = new JButton("OPEN");
 
-        addButton.setPreferredSize(new Dimension(150,50));
-        removeButton.setPreferredSize(new Dimension(150,50));
-        openButton.setPreferredSize(new Dimension(150,50));
+        addButton.setPreferredSize(new Dimension(175, 50));
+        removeButton.setPreferredSize(new Dimension(175, 50));
+        openButton.setPreferredSize(new Dimension(175, 50));
 
-        add(new JLabel("Cerca:"),"right");
+        /*Test recipe list*/
+        ArrayList<Recipe> testRecipeList = new ArrayList<>();
+
+        testRecipeList.add(new Recipe());
+        testRecipeList.add(new Recipe());
+        testRecipeList.add(new Recipe());
+        /*end test*/
+
+        Object[][] recipeMatrix = new Object[testRecipeList.size()][5];
+
+        for (int i = 0; i < testRecipeList.size(); ++i) {
+            recipeMatrix[i][0] = testRecipeList.get(i).getName();
+            recipeMatrix[i][1] = 1000;
+            recipeMatrix[i][2] = testRecipeList.get(i).getPreptime();
+            recipeMatrix[i][3] = testRecipeList.get(i).getCooktime();
+            recipeMatrix[i][4] = testRecipeList.get(i).getId();
+        }
+
+        DefaultTableModel recipeModel = new DefaultTableModel(recipeMatrix, new String[]{"Nome", "kCal", "Tempo Preparazione", "Tempo Cottura"});
+
+        JTable recipeTable = new JTable(recipeModel);
+        JScrollPane scrollPanel = new JScrollPane(recipeTable);
+        scrollPanel.setBorder(BorderFactory.createTitledBorder("Ricette"));
+
+        add(new JLabel("Cerca:"), "right");
         add(new JTextField(""), "");
         add(new JButton("Filtri"));
         add(new JLabel("Ordina Per:"), "top");
-        add(new JPanel(new GridLayout(1,8)));
+        add(new JPanel(new GridLayout(1, 8)));
         add(new JLabel(""));
-        add(new JTextPane(),"span 2 4, grow");
-        add(new JLabel(""),"");
-        add(addButton,"right");
-        add(removeButton,"right");
-        add(openButton,"right");
+        add(scrollPanel, "span 2 4, grow");
+        add(new JLabel(""), "");
+        add(addButton, "right");
+        add(removeButton, "right");
+        add(openButton, "right");
 
     }
 
--- src/launchcode/Main.java
+++ src/launchcode/Main.java
@@ -3,6 +3,8 @@ package launchcode;
 import domainclasses.recipes.*;
 import graphicalinterface.*;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
 
@@ -13,6 +15,8 @@ public class Main {
         IngredientQty d = new IngredientQty(b,500);
 
         Recipe e = new Recipe();
+
+
 
         System.out.println(a);
         System.out.println(b);
