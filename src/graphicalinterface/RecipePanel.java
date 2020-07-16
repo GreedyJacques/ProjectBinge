fe30641269df64ff73cba4b3e3a5a817688a4ae1
Vaidas Lenartavicius <vaidas.lenartavicius@gmail.com>
2020-07-16 03:04:24 +0200

    started RecipePanel placeholder implementation

--- src/graphicalinterface/RecipePanel.java
+++ src/graphicalinterface/RecipePanel.java
@@ -1,24 +1,36 @@
 package graphicalinterface;
 
+import net.miginfocom.swing.MigLayout;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class RecipePanel extends JSplitPane implements ActionListener {
+public class RecipePanel extends JPanel implements ActionListener {
 
     public RecipePanel() {
-        super(JSplitPane.VERTICAL_SPLIT,new JPanel(),new JPanel());
-        JTextPane p1 = new JTextPane();
-        JButton p2 = new JButton("Ciao");
+        super(new MigLayout("fill, wrap 3", "50[][grow,fill]20[]","50[][]20[grow,fill][][][]50"));
 
-        setTopComponent(p1);
-        setBottomComponent(p2);
+        JButton addButton = new JButton("ADD");
+        JButton removeButton = new JButton("REMOVE");
+        JButton openButton = new JButton("OPEN");
 
-        setDividerLocation(360);
-        setDividerSize(0);
+        addButton.setPreferredSize(new Dimension(150,50));
+        removeButton.setPreferredSize(new Dimension(150,50));
+        openButton.setPreferredSize(new Dimension(150,50));
 
-        setResizeWeight(1.0);
+        add(new JLabel("Cerca:"),"right");
+        add(new JTextField(""), "");
+        add(new JButton("Filtri"));
+        add(new JLabel("Ordina Per:"), "top");
+        add(new JPanel(new GridLayout(1,8)));
+        add(new JLabel(""));
+        add(new JTextPane(),"span 2 4,grow");
+        add(new JLabel(""),"grow");
+        add(addButton,"right");
+        add(removeButton,"right");
+        add(openButton,"right");
 
     }
 
